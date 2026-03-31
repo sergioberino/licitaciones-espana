f59601fffb4f77bb5265a36f4189b68106b9536e
Daniel Teres <daniel.teres@tdconsulting.es>
2026-03-31 12:28:29 +0200

    Nuevo nombre sql subvenciones

---
+++ schemas/012_nacional_subvenciones.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS l0.nacional_subvenciones (
+  id INTEGER PRIMARY KEY,
+  numeroConvocatoria VARCHAR(255) NOT NULL,
+  mrr BOOLEAN,
+  descripcion TEXT,
+  descripcionLeng TEXT,
+  fechaRecepcion DATE,
+  nivel1 VARCHAR(255),
+  nivel2 VARCHAR(255),
+  nivel3 VARCHAR(255),
+  codigoINVENTE VARCHAR(255)
+);
